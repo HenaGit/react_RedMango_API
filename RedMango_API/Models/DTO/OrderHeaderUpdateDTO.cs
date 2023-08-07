ab0fa58c8fe483a6a8e54eb72842f46152a437ba
HenokG <heni.gebrehiwot@gmail.com>
2023-08-07 12:41:17 -0600

    Order DTOs

---
+++ RedMango_API/Models/DTO/OrderHeaderUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace RedMango_API.Models.DTO
+{
+    public class OrderHeaderUpdateDTO
+    {
+        public int OrderHeaderId { get; set; }
+        public string PickupName { get; set; }
+        public string PickupPhoneNumber { get; set; }
+        public string PickupEmail { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntentID { get; set; }
+        public string Status { get; set; }
+    }
+}
